7b38d6a072e55fcfb45c7ffbb07efd5b554f0a0a
Dan1aR <andan.com@mail.ru>
2021-12-13 01:58:05 +0300

    Hopefully final commit

--- test.rb
+++ test.rb
@@ -7,36 +7,28 @@ require_relative 'funcmodule'
 class TestRobot < Minitest::Test
   def test_circle_area3
     area = ->(r) { Math::PI * (r**2) }
-    (1..100).step(0.2).each do |x|
-      assert_in_delta(area.call(x), circle_area(x, 10**-3), 10**-3)
-    end
+    assert_in_delta(area.call(10), circle_area(10, 10**-3), 10**-3)
   end
 
   def test_circle_area3_adv
     area = ->(r) { Math::PI * (r**2) }
-    (1..100).step(0.2).each do |x|
-      assert_in_delta(area.call(x), circle_area_advanced(x, 10**-3), 10**-3)
-    end
+    assert_in_delta(area.call(10), circle_area_advanced(10, 10**-3), 10**-3)
   end
 
   def test_circle_area4
     area = ->(r) { Math::PI * (r**2) }
-    (1..100).step(0.2).each do |x|
-      assert_in_delta(area.call(x), circle_area(x, 10**-4), 10**-4)
-    end
+    assert_in_delta(area.call(10), circle_area(10, 10**-4), 10**-4)
   end
 
   def test_circle_area4_adv
     area = ->(r) { Math::PI * (r**2) }
-    (1..100).step(0.2).each do |x|
-      assert_in_delta(area.call(x), circle_area_advanced(x, 10**-4), 10**-4)
-    end
+    assert_in_delta(area.call(10), circle_area_advanced(10, 10**-4), 10**-4)
   end
 
   def test_proc
     mxl = maximb(&proc {
       max_dist = 0.0
-      (0.5..1).step(0.01).each do |x|
+      (0.5..1).step(0.01) do |x|
         max_dist = [max_dist, (Math.sin(x) / x - Math.tan(x + 1) / (x + 1)).abs].max
       end
       max_dist
@@ -48,7 +40,7 @@ class TestRobot < Minitest::Test
   def test_bloc
     mxb = maximb do
       max_dist = 0.0
-      (0.5..1).step(0.01).each do |x|
+      (0.5..1).step(0.01) do |x|
         max_dist = [max_dist, (Math.sin(x) / x - Math.tan(x + 1) / (x + 1)).abs].max
       end
       max_dist
